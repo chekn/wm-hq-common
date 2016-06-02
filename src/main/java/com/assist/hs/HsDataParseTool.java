1b62313d05c8541674708bec8c6e3423c85ca878
chekn <chekn_auto@163.com>
2016-06-02 16:23:30 +0800

    add tool class

---
+++ src/main/java/com/assist/hs/HsDataParseTool.java
@@ -0,0 +1,38 @@
+package com.assist.hs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author Pactera-NEN
+ * @date 2016年6月2日-下午2:59:35
+ */
+public class HsDataParseTool {
+	
+	/**
+	 * HQ OriData parse
+	 * @param oriData
+	 * @return
+	 */
+	public static Map<String, Map<String,Object>> parseHqOriData(Map<String, List<Object>> oriData){
+		Map<String, Map<String,Object>> rev=new HashMap<String, Map<String,Object>>();
+		
+		List<Object> fieldNames=oriData.get("fields");
+		int fieldCount=fieldNames.size();
+		for(String key:oriData.keySet()){
+			if(key.equals("fields")) continue;
+			
+			Map<String,Object> pVal=new HashMap<String,Object>();
+			List<Object> oVal=oriData.get(key);
+			for(int i=0; i<fieldCount; i++){
+				pVal.put((String)fieldNames.get(i), oVal.get(i));
+			}
+			rev.put(key, pVal);
+		}
+		
+		return rev;
+	}
+
+}
